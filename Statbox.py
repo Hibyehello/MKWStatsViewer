28f1047fbb8f3c16000b22b52aba3f7abce724c4
vabold <vabold820@gmail.com>
2021-11-29 08:36:20 -0500

    Minor UI fixes

--- Statbox.py
+++ Statbox.py
@@ -2,13 +2,15 @@ from PyQt5.QtWidgets import *
 from PyQt5.QtGui import *
 from PyQt5.QtCore import *
 
+
 class StatBox:
     def __init__(self, index):
         self.index = index
-
+        self.line = QFrame()
 
     def stats(self, current_displayed):
 
+        # Add Tooltips here
         self.numWheelTT = "Number of tires:\n0 = 4 tires\n1 = 2 tires\n2 = 2 tires, closer to each other (used on Quacker)\n3 = 3 tires (used on Blue Falcon)."
         self.DriftTypeTT = "Drift type:\n0 = outside drift used on karts\n1 = outside drift used on bikes\n2 = inside drift"
         self.WeightClassTT = "Weight class (affects camera and trick rotation notably)\n0 = light\n1 = medium\n2 = heavy"
@@ -26,7 +28,6 @@ class StatBox:
         # Add Widgets here
         self.GeneralLabel = QLabel("General")
         self.GeneralLabel.setFont(self.Font)
-        self.GeneralLabel.setMaximumHeight(20)
 
         self.numWheelLabel = QLabel("# of Wheels:")
         self.numWheelText = QLineEdit(str(current_displayed[0]))
@@ -43,7 +44,6 @@ class StatBox:
         self.WeightClassLabel.setToolTip(self.WeightClassTT)
         self.WeightClassText.setToolTip(self.WeightClassTT)
 
-
         self.SpeedLabel = QLabel("Speed:")
         self.SpeedText = QLineEdit(str(current_displayed[9]))
 
@@ -53,21 +53,26 @@ class StatBox:
         self.SpeedInTurnLabel = QLabel("Speed in turn: (%)")
         self.SpeedInTurnText = QLineEdit(str(current_displayed[10]))
 
+        # Create a horizontal divider line
+        self.line.setFrameShape(QFrame.HLine)
+        self.line.setFrameShadow(QFrame.Sunken)
 
         # Add Widgets to the layout
         self.layout.addWidget(self.GeneralLabel, 1, 0)
-        self.layout.addWidget(self.SpeedLabel, 2, 0)
-        self.layout.addWidget(self.SpeedText, 2, 1)
-        self.layout.addWidget(self.WeightLabel, 3, 0)
-        self.layout.addWidget(self.WeightText, 3, 1)
-        self.layout.addWidget(self.SpeedInTurnLabel, 4, 0)
-        self.layout.addWidget(self.SpeedInTurnText, 4, 1)
-        self.layout.addWidget(self.numWheelLabel, 5, 0)
-        self.layout.addWidget(self.numWheelText, 5, 1)
-        self.layout.addWidget(self.DriftTypeLabel, 6, 0)
-        self.layout.addWidget(self.DriftTypeText, 6, 1)
-        self.layout.addWidget(self.WeightClassLabel, 7, 0)
-        self.layout.addWidget(self.WeightClassText, 7, 1)
+        self.layout.addWidget(self.line, 2, 0)
+        self.layout.addWidget(self.line, 2, 1)
+        self.layout.addWidget(self.SpeedLabel, 3, 0)
+        self.layout.addWidget(self.SpeedText, 3, 1)
+        self.layout.addWidget(self.WeightLabel, 4, 0)
+        self.layout.addWidget(self.WeightText, 4, 1)
+        self.layout.addWidget(self.SpeedInTurnLabel, 5, 0)
+        self.layout.addWidget(self.SpeedInTurnText, 5, 1)
+        self.layout.addWidget(self.numWheelLabel, 6, 0)
+        self.layout.addWidget(self.numWheelText, 6, 1)
+        self.layout.addWidget(self.DriftTypeLabel, 7, 0)
+        self.layout.addWidget(self.DriftTypeText, 7, 1)
+        self.layout.addWidget(self.WeightClassLabel, 8, 0)
+        self.layout.addWidget(self.WeightClassText, 8, 1)
 
         # Set editWidget's layout to self.layout
         self.pLayout.setAlignment(Qt.AlignTop)
