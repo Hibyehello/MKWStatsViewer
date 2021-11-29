7b6c41ec7760bf8196b85bf1a0f69dbff560b69f
Hibyehello <joshuadcox05@gmail.com>
2021-11-28 18:48:43 -0600

    Fix layout in Statbox.py

--- Statbox.py
+++ Statbox.py
@@ -2,7 +2,6 @@ from PyQt5.QtWidgets import *
 from PyQt5.QtGui import *
 from PyQt5.QtCore import *
 
-
 class StatBox:
     def __init__(self, index):
         self.index = index
@@ -12,6 +11,7 @@ class StatBox:
         # Define main Widget and Layout
         self.editWidget = QWidget()
         self.layout = QGridLayout()
+        self.pLayout = QGridLayout()
 
         # Font for some Category Labels
         self.Font = QFont()
@@ -21,6 +21,7 @@ class StatBox:
         # Add Widgets here
         self.GeneralLabel = QLabel("General")
         self.GeneralLabel.setFont(self.Font)
+        self.GeneralLabel.setMaximumHeight(20)
         self.SpeedLabel = QLabel("Speed:")
         self.SpeedText = QLineEdit(str(current_displayed[9]))
 
@@ -30,16 +31,19 @@ class StatBox:
         self.SpeedInTurnLabel = QLabel("Speed in turn: (%)")
         self.SpeedInTurnText = QLineEdit(str(current_displayed[10]))
 
+
         # Add Widgets to the layout
-        self.layout.addWidget(self.GeneralLabel, 0, 0)
-        self.layout.addWidget(self.SpeedLabel, 1, 0)
-        self.layout.addWidget(self.SpeedText, 1, 1)
-        self.layout.addWidget(self.WeightLabel, 2, 0)
-        self.layout.addWidget(self.WeightText, 2, 1)
-        self.layout.addWidget(self.SpeedInTurnLabel, 3, 0)
-        self.layout.addWidget(self.SpeedInTurnText, 3, 1)
+        self.layout.addWidget(self.GeneralLabel, 1, 0)
+        self.layout.addWidget(self.SpeedLabel, 2, 0)
+        self.layout.addWidget(self.SpeedText, 2, 1)
+        self.layout.addWidget(self.WeightLabel, 3, 0)
+        self.layout.addWidget(self.WeightText, 3, 1)
+        self.layout.addWidget(self.SpeedInTurnLabel, 4, 0)
+        self.layout.addWidget(self.SpeedInTurnText, 4, 1)
 
         # Set editWidget's layout to self.layout
-        self.editWidget.setLayout(self.layout)
+        self.pLayout.setAlignment(Qt.AlignTop)
+        self.pLayout.addLayout(self.layout, 0, 0)
+        self.editWidget.setLayout(self.pLayout)
 
         return self.editWidget
