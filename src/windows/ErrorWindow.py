c635570173a31bf0f3832478b01ac901b8596249
Josh <36666883+Hibyehello@users.noreply.github.com>
2022-01-10 07:40:22 -0600

    Fix ErrorWindow

--- src/windows/ErrorWindow.py
+++ src/windows/ErrorWindow.py
@@ -3,11 +3,11 @@ from PyQt5.QtWidgets import QMessageBox
 errlist = ["kartParam.bin and/or driverParam.bin are modded"]
 
 
-def error():
+def error(index):
     msg = QMessageBox()
     msg.setIcon(QMessageBox.Critical)
     msg.setStyleSheet("QLabel{min-width: 150px;}")
     msg.setText("Error")
-    msg.setInformativeText(errlist[0])
+    msg.setInformativeText(errlist[index])
     msg.setWindowTitle("Error")
     msg.exec_()
